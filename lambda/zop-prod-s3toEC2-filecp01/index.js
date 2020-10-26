ab3b092cd5ae192f6127542b57a7636b736a1f50
tani <tani@j-wing.jp>
2020-10-26 17:52:25 +0900

    2つあるS3への対応

--- lambda/zop-prod-s3toEC2-filecp01/index.js
+++ lambda/zop-prod-s3toEC2-filecp01/index.js
@@ -2,14 +2,10 @@
 exports.handler = async (event, context) => {
   // S3 のインスタンス名
   var bucket = event.Records[0].s3.bucket.name;
-  // key（PUTのイベントがあったファイル名）
+  // key（アップロードのイベントがあったファイル名）
   var key = event.Records[0].s3.object.key;
   
-  var sqs = require('./sqs.js');
   var ec2 = require('./ec2.js');
-  
-  // SQSメッセージ送信
-  sqs.queueing(key);
   
   // EC2へZIPファイル送信＆解凍
   return ec2.download(bucket, key);
