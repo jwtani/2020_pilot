17ad9d0b041430c77efb24c80827dd8e89ca44c2
tani <tani@j-wing.jp>
2022-02-04 14:35:57 +0900

    諸々修正

--- subversion/tmp2020/ansible/db/roles/shell/files/insurance_file_convert.sh
+++ subversion/tmp2020/ansible/db/roles/shell/files/insurance_file_convert.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
+set -u
+
+DAIDO_CODE="00001"
 
 BASE_DIR=/data/jmc
 TRANS_DIR=${BASE_DIR}/jmc_w_ins_trans
 
-DAIDO=00001
+for inputfile in `find ${TRANS_DIR} -regextype posix-basic -regex '^'${TRANS_DIR}'/[0-9]\{6\}/[0-9]\{5\}/[0-9]\{5\}_[0-9]\{4\}\.txt$'`; do
 
-for FILE in `find $TRANS_DIR -regextype posix-basic -regex '^'${TRANS_DIR}'/[0-9]\{6\}/[0-9]\{5\}/[0-9]\{5\}_[0-9]\{4\}\.txt$'`; do
+  ym=`echo ${inputfile} | cut -d"/" -f5`
+  ins=`echo ${inputfile} | cut -d"/" -f6`
 
-  MONTH=`echo $FILE | cut -d"/" -f5`
-  INSNO=`echo $FILE | cut -d"/" -f6`
+  file_name=$(basename ${inputfile})
 
-  WORK_DIR=${TRANS_DIR}/${MONTH}/${INSNO}
-  CMT_DIR=${BASE_DIR}/jmc_w_ins_cmt/${MONTH}/${INSNO}
+  # 例：00001_0001.txt → 0001.txt
+  outputfile_name=`echo ${file_name} | cut -d"_" -f2`
+  tempfile=${TRANS_DIR}/${ym}/${ins}/${outputfile_name}
 
-  FILE_NAME=`echo $FILE | cut -d"/" -f7`
-
-  FILE_SJIS=${WORK_DIR}/sjis.txt
-  FILE_FIN=`echo $FILE_NAME | cut -d"_" -f2`
-  FILE_LF=${WORK_DIR}/${FILE_FIN}
-
-  iconv -f 'IBM930' -t 'IBM-943' $FILE -o $FILE_SJIS
-
-  if [ $INSNO = $DAIDO ]; then
-    fold -bw 600 $FILE_SJIS > $FILE_LF
-  else
-    fold -bw 300 $FILE_SJIS > $FILE_LF
+  # 横幅を変更
+  bw=300
+  if [ ${ins} = ${DAIDO_CODE} ]; then
+    bw=600
   fi
 
-  echo "" >> $FILE_LF
-  unix2dos $FILE_LF
-  cp $FILE_LF ${CMT_DIR}/${FILE_FIN}
-  rm $FILE $FILE_SJIS $FILE_LF 
+  # 文字コード変換
+  iconv -f 'IBM930' -t 'IBM-943' ${inputfile} | fold -bw ${bw} > ${tempfile}
+  echo "" >> ${tempfile}
+  unix2dos ${tempfile}
+
+  # ファイル移動
+  outputfile=${BASE_DIR}/jmc_w_ins_cmt/${ym}/${ins}/${outputfile_name}
+  mv ${tempfile} ${outputfile} && rm ${inputfile}
 done
+
 exit 0
