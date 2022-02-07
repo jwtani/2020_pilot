7bacd139ae276a1938d0001348bb164081c9c1bd
tani <tani@j-wing.jp>
2022-02-07 15:29:12 +0900

    メール通知

--- subversion/tmp2020/ansible/db/roles/shell/files/insurance_file_move.sh
+++ subversion/tmp2020/ansible/db/roles/shell/files/insurance_file_move.sh
@@ -3,25 +3,56 @@ set -u
 umask 007
 
 # 保険会社コード
-ins_code=$1
+INS_CODE=$1
+
 # "ebcdic" | "ebcdic_old" | その他
-char_code=$2
+CHAR_CODE=$2
 
-ym=`date +%Y%m`
-infile=/data/jmc/${ins_code}.txt
-outdir=/data/jmc/jmc_w_ins_trans/${ym}/${ins_code}
-tmpfile=${outdir}/${ins_code}
-outfile=${tmpfile}.txt
+# メール送信先アドレス
+#TO_ADDR=hoken@zen-ikyo.or.jp
+TO_ADDR=tani@j-wing.jp
 
-case "${char_code}" in
-    ebcdic)
-        iconv -f IBM930 -t UTF-8 ${infile} | nkf -x --windows > ${tmpfile}
-        ;;
-    ebcdic_old)       
-        iconv -f EBCDIC-JP-KANA -t UTF-8 ${infile} | nkf -s > ${tmpfile}
-        ;;
-    *)
-        cp /data/jmc/$1.txt $FILE.txt
-        ;;
-esac
+# 全銀ミドル出力ファイルを変換し移動
+file_trans_cp932() {
+    local inscode=$1
+    local charcode=$2
+    local ym=`date +%Y%m`
+    local infile=/data/jmc/${inscode}.txt
+    local outdir=/data/jmc/jmc_w_ins_trans/${ym}/${inscode}
+    local tmpfile=${outdir}/${inscode}
+    local outfile=${tmpfile}.txt
+
+    case "${charcode}" in
+        ebcdic)
+            iconv -f IBM930 -t UTF-8 ${infile} | nkf -x --windows > ${tmpfile}
+            ;;
+        ebcdic_old)
+            iconv -f EBCDIC-JP-KANA -t UTF-8 ${infile} | nkf -s > ${tmpfile}
+            ;;
+        *)
+            cp ${infile} ${tmpfile}
+            ;;
+    esac
+}
+
+# メール送信
+send_mail_insurance() {
+    local toaddr=$1
+    local inscode=$2
+    local subject
+    local ym ymstr
+
+    # 処理年月取得 eg. 2022年2月
+    ym=`date +%Y年%m月`
+    ymstr=${ym/年0/年}
+    subject="${ymstr}分保険会社[${inscode}]請求ファイル受信"
+    body="保険会社[${inscode}]から${ymstr}分の保険請求ファイルを受信しました。"
+
+    echo "${body}" | mutt -s "${subject}" ${toaddr}
+    return $?
+}
+
+file_trans_cp932 ${INS_CODE} ${CHAR_CODE}
+
+send_mail_insurance ${TO_ADDR} ${INS_CODE}
 
