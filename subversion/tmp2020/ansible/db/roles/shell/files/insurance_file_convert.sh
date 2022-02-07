e3a6ee175d4da91aa6922a94864e42cae55116bb
tani <tani@j-wing.jp>
2022-02-07 15:39:45 +0900

    変数名修正

--- subversion/tmp2020/ansible/db/roles/shell/files/insurance_file_convert.sh
+++ subversion/tmp2020/ansible/db/roles/shell/files/insurance_file_convert.sh
@@ -6,16 +6,16 @@ DAIDO_CODE="00001"
 BASE_DIR=/data/jmc
 TRANS_DIR=${BASE_DIR}/jmc_w_ins_trans
 
-for inputfile in `find ${TRANS_DIR} -regextype posix-basic -regex '^'${TRANS_DIR}'/[0-9]\{6\}/[0-9]\{5\}/[0-9]\{5\}_[0-9]\{4\}\.txt$'`; do
+for infile in `find ${TRANS_DIR} -regextype posix-basic -regex '^'${TRANS_DIR}'/[0-9]\{6\}/[0-9]\{5\}/[0-9]\{5\}_[0-9]\{4\}\.txt$'`; do
 
-  ym=`echo ${inputfile} | cut -d"/" -f5`
-  ins=`echo ${inputfile} | cut -d"/" -f6`
+  ym=`echo ${infile} | cut -d"/" -f5`
+  ins=`echo ${infile} | cut -d"/" -f6`
 
-  file_name=$(basename ${inputfile})
+  file_name=$(basename ${infile})
 
   # 例：00001_0001.txt → 0001.txt
-  outputfile_name=`echo ${file_name} | cut -d"_" -f2`
-  tempfile=${TRANS_DIR}/${ym}/${ins}/${outputfile_name}
+  outfile_name=`echo ${file_name} | cut -d"_" -f2`
+  tmpfile=${TRANS_DIR}/${ym}/${ins}/${outfile_name}
 
   # 横幅を変更
   bw=300
@@ -24,13 +24,13 @@ for inputfile in `find ${TRANS_DIR} -regextype posix-basic -regex '^'${TRANS_DIR
   fi
 
   # 文字コード変換
-  iconv -f 'IBM930' -t 'IBM-943' ${inputfile} | fold -bw ${bw} > ${tempfile}
-  echo "" >> ${tempfile}
-  unix2dos ${tempfile}
+  iconv -f 'IBM930' -t 'IBM-943' ${infile} | fold -bw ${bw} > ${tmpfile}
+  echo "" >> ${tmpfile}
+  unix2dos ${tmpfile}
 
   # ファイル移動
-  outputfile=${BASE_DIR}/jmc_w_ins_cmt/${ym}/${ins}/${outputfile_name}
-  mv ${tempfile} ${outputfile} && rm ${inputfile}
+  outfile=${BASE_DIR}/jmc_w_ins_cmt/${ym}/${ins}/${outfile_name}
+  mv ${tmpfile} ${outfile} && rm ${infile}
 done
 
 exit 0
