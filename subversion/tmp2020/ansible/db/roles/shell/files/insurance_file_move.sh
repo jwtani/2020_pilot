eee46c36acf8e346533d3b337f055b6644b2ed20
tani <tani@j-wing.jp>
2022-02-07 13:31:31 +0900

    リファクタ

--- subversion/tmp2020/ansible/db/roles/shell/files/insurance_file_move.sh
+++ subversion/tmp2020/ansible/db/roles/shell/files/insurance_file_move.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
-umask 003
-#YM=`ls -D /data/jmc/jmc_w_ins_trans/  | tr -d /`
-YM=`date +%Y%m`
-FILE=/data/jmc/jmc_w_ins_trans/$YM/$1/$1
+set -u
+umask 007
 
-case "$2" in
+# 保険会社コード
+ins_code=$1
+# "ebcdic" | "ebcdic_old" | その他
+char_code=$2
+
+ym=`date +%Y%m`
+infile=/data/jmc/${ins_code}.txt
+outdir=/data/jmc/jmc_w_ins_trans/${ym}/${ins_code}
+tmpfile=${outdir}/${ins_code}
+outfile=${tmpfile}.txt
+
+case "${char_code}" in
     ebcdic)
-        #mv /data/jmc/$1.txt $FILE.ebcdic
-        cp /data/jmc/$1.txt $FILE.ebcdic
-        iconv -f IBM930 -t UTF-8 $FILE.ebcdic > $FILE.utf
-        nkf -x --windows $FILE.utf > $FILE.txt
-        rm $FILE.ebcdic
-        rm $FILE.utf
+        iconv -f IBM930 -t UTF-8 ${infile} | nkf -x --windows > ${tmpfile}
         ;;
     ebcdic_old)       
-        #mv /data/jmc/$1.txt $FILE.ebcdic
-        cp /data/jmc/$1.txt $FILE.ebcdic
-        iconv -f EBCDIC-JP-KANA -t UTF-8 $FILE.ebcdic > $FILE.utf
-        nkf -s $FILE.utf > $FILE.txt
-        rm $FILE.ebcdic
-        rm $FILE.utf
+        iconv -f EBCDIC-JP-KANA -t UTF-8 ${infile} | nkf -s > ${tmpfile}
         ;;
     *)
-        #mv /data/jmc/$1.txt $FILE.txt
         cp /data/jmc/$1.txt $FILE.txt
         ;;
 esac
 
-chmod 770 /data/jmc/jmc_w_ins_trans/$YM/$1/$1.txt
